3e0a2d68ce1a42eb2be1c0bb36c9956fff08947f
Mike Lapshin <sotakone@sotakone.com>
2010-07-01 14:27:01 +0400

    foboar

--- src/session_tab.cpp
+++ src/session_tab.cpp
@@ -40,7 +40,9 @@ void SessionTab::visit(const QString& url)
 
 void SessionTab::waitForLoad()
 {
+  pageLoadedMutex.lock();
   pageLoaded.wait(&pageLoadedMutex);
+  pageLoadedMutex.unlock();
 }
 
 void SessionTab::updateTitle(const QString& t)
